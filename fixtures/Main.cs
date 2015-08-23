ed1b28acd57eefb84aaba480a3b731b0fb41b8f7
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2015-08-23 20:44:33 +0700

    Fix wrong color in c# and javascript. Add fixtures for test color

---
+++ fixtures/Main.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class AppMain {
+
+    private string field1;
+
+    public Object obj;
+
+    /// <summary>
+    ///   The main entry point for the application
+    /// </summary>
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        Console.Writeln("Hello World");
+    }
+}
---
+++ fixtures/main.js
@@ -0,0 +1,17 @@
+// this is comment
+
+/*
+ * this is comment
+ */
+
+var hello = (function() {
+  function Main() {
+
+  }
+
+  Main.prototype.method = function(arg) {
+    return "hello world";
+  };
+
+  return Main;
+});
